6390677cf55be49978214e2d37bf9006a42672e2
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-05 09:05:30 +0430

    fixed the stuck form input

--- src/App2.js
+++ src/App2.js
@@ -1,43 +1,38 @@
 import { useState } from "react";
-import { sculptureList } from "./data.js";
 
-export default function Gallery() {
-  let [index, setIndex] = useState(0);
-  const [showMore, setShowMore] = useState(false);
+export default function Form() {
+  let [firstName, setFirstName] = useState("");
+  let [lastName, setLastName] = useState("");
 
-  function handleNextClick() {
-    if (index < sculptureList.length - 1) {
-      setIndex(index + 1);
-    }
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
   }
 
-  function handlePreviousClick() {
-    if (index > 0) {
-      setIndex(index - 1);
-    }
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
   }
 
-  function handleMoreClick() {
-    setShowMore(!showMore);
+  function handleReset() {
+    setFirstName("");
+    setLastName("");
   }
 
-  let sculpture = sculptureList[index];
   return (
-    <>
-      <button onClick={handleNextClick}>Next</button>
-      <button onClick={handlePreviousClick}>Previous</button>
-      <h2>
-        <i>{sculpture.name} </i>
-        by {sculpture.artist}
-      </h2>
-      <h3>
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <button onClick={handleMoreClick}>
-        {showMore ? "Hide" : "Show"} details
-      </button>
-      {showMore && <p>{sculpture.description}</p>}
-      <img src={sculpture.url} alt={sculpture.alt} />
-    </>
+    <form onSubmit={(e) => e.preventDefault()}>
+      <input
+        placeholder="First name"
+        value={firstName}
+        onChange={handleFirstNameChange}
+      />
+      <input
+        placeholder="Last name"
+        value={lastName}
+        onChange={handleLastNameChange}
+      />
+      <h1>
+        Hi, {firstName} {lastName}
+      </h1>
+      <button onClick={handleReset}>Reset</button>
+    </form>
   );
 }
