0a77ddab80a6302eb4992c69abae69a2361377fc
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-06 12:36:36 +0430

    using immer for mutable change

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import ReactDev from "./ReactDev";
-import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import App2 from "./App2";
 
