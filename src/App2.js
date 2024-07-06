72da4bc50e7bd94f2cc23686fc89528fccea612e
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-06 20:03:45 +0430

    challenge- find and fix the mutation

--- src/App2.js
+++ src/App2.js
@@ -1,49 +1,121 @@
 import { useState } from "react";
 
-export default function Scoreboard() {
-  const [player, setPlayer] = useState({
-    firstName: "Ranjani",
-    lastName: "Shettar",
-    score: 10,
+const initialPosition = {
+  x: 10,
+  y: 10,
+};
+
+export default function Canvas() {
+  const [shape, setShape] = useState({
+    color: "orange",
+    position: initialPosition,
   });
 
-  function handlePlusClick() {
-    setPlayer({
-      ...player,
-      score: player.score + 1,
+  function handleMove(dx, dy) {
+    setShape({
+      ...shape,
+      position: {
+        x: shape.position.x + dx,
+        y: shape.position.y + dy,
+      },
     });
+    // shape.position.x += dx;
+    // shape.position.y += dy;
   }
 
-  function handleFirstNameChange(e) {
-    setPlayer({
-      ...player,
-      firstName: e.target.value,
-    });
-  }
-
-  function handleLastNameChange(e) {
-    setPlayer({
-      ...player,
-      lastName: e.target.value,
+  function handleColorChange(e) {
+    setShape({
+      ...shape,
+      color: e.target.value,
     });
   }
 
   return (
     <>
-      <label>
-        Score: <b>{player.score}</b>{" "}
-        <button onClick={handlePlusClick}>+1</button>
-      </label>
-      <br />
-      <label>
-        First name:
-        <input value={player.firstName} onChange={handleFirstNameChange} />
-      </label>
-      <br />
-      <label>
-        Last name:
-        <input value={player.lastName} onChange={handleLastNameChange} />
-      </label>
+      <select value={shape.color} onChange={handleColorChange}>
+        <option value="orange">orange</option>
+        <option value="lightpink">lightpink</option>
+        <option value="aliceblue">aliceblue</option>
+      </select>
+      <Background position={initialPosition} />
+      <Box color={shape.color} position={shape.position} onMove={handleMove}>
+        Drag me!
+      </Box>
     </>
+  );
+}
+
+////////////////////////////////////
+
+function Box({ children, color, position, onMove }) {
+  const [lastCoordinates, setLastCoordinates] = useState(null);
+
+  function handlePointerDown(e) {
+    e.target.setPointerCapture(e.pointerId);
+    // console.log(e.target.setPointerCapture(e.pointerId));
+    setLastCoordinates({
+      x: e.clientX,
+      y: e.clientY,
+    });
+  }
+
+  function handlePointerMove(e) {
+    if (lastCoordinates) {
+      setLastCoordinates({
+        x: e.clientX,
+        y: e.clientY,
+      });
+      const dx = e.clientX - lastCoordinates.x;
+      const dy = e.clientY - lastCoordinates.y;
+      onMove(dx, dy);
+    }
+  }
+
+  function handlePointerUp(e) {
+    setLastCoordinates(null);
+  }
+
+  return (
+    <div
+      onPointerDown={handlePointerDown}
+      onPointerMove={handlePointerMove}
+      onPointerUp={handlePointerUp}
+      style={{
+        width: 100,
+        height: 100,
+        cursor: "grab",
+        backgroundColor: color,
+        position: "absolute",
+        border: "1px solid black",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        transform: `translate(
+          ${position.x}px,
+          ${position.y}px
+        )`,
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+/////////////////////////////////////
+
+function Background({ position }) {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        transform: `translate(
+        ${position.x}px,
+        ${position.y}px
+      )`,
+        width: 250,
+        height: 250,
+        backgroundColor: "rgba(200, 200, 0, 0.2)",
+      }}
+    />
   );
 }
