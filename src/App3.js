62eccac447f97c836684a9b5f5e2afb67e6f3adc
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-08 09:34:11 +0430

    challenge one using immer

--- src/App3.js
+++ src/App3.js
@@ -20,19 +20,24 @@ const initialProducts = [
 ];
 
 export default function ShoppingCart() {
-  const [products, setProducts] = useState(initialProducts);
-  // const [products, updateProducts] = useImmer(initialProducts);
+  // const [products, setProducts] = useState(initialProducts);
+  const [products, updateProducts] = useImmer(initialProducts);
 
   function handleIncreaseClick(productId) {
-    setProducts(
-      products.map((product) => {
-        if (product.id === productId) {
-          return { ...product, count: product.count + 1 };
-        } else {
-          return product;
-        }
-      })
-    );
+    updateProducts((draft) => {
+      const nextProduct = draft.find((a) => a.id === productId);
+      nextProduct.count += +1;
+    });
+
+    // setProducts(
+    //   products.map((product) => {
+    //     if (product.id === productId) {
+    //       return { ...product, count: product.count + 1 };
+    //     } else {
+    //       return product;
+    //     }
+    //   })
+    // );
   }
 
   return (
