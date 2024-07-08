ad83ee6a5f34457f7d6ee6bce3860834600a0d27
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-08 09:26:58 +0430

    challenge one

--- src/App3.js
+++ src/App3.js
@@ -1,100 +1,52 @@
 import { useState } from "react";
 import { useImmer } from "use-immer";
 
-const initialList = [
-  { id: 0, title: "Big Bellies", seen: false },
-  { id: 1, title: "Lunar Landscape", seen: false },
-  { id: 2, title: "Terracotta Army", seen: true },
+const initialProducts = [
+  {
+    id: 0,
+    name: "Baklava",
+    count: 1,
+  },
+  {
+    id: 1,
+    name: "Cheese",
+    count: 5,
+  },
+  {
+    id: 2,
+    name: "Spaghetti",
+    count: 2,
+  },
 ];
 
-export default function BucketList() {
-  // const [myList, setMyList] = useState(initialList);
-  // const [myList2, setMyList2] = useState(initialList);
+export default function ShoppingCart() {
+  const [products, setProducts] = useState(initialProducts);
+  // const [products, updateProducts] = useImmer(initialProducts);
 
-  const [myList, updateMyList] = useImmer(initialList);
-  const [myList2, updateMyList2] = useImmer(initialList);
-
-  function handleToggleMyList(artworkId, nextSeen) {
-    updateMyList((draft) => {
-      const artwork = draft.find((a) => a.id === artworkId);
-      artwork.seen = nextSeen;
-    });
-
-    // setMyList(
-    //   myList.map((artwork) => {
-    //     if (artwork.id === artworkId) {
-    //       // create a new object with changes
-    //       return { ...artwork, seen: nextSeen };
-    //     } else {
-    //       // no changes
-    //       return artwork;
-    //     }
-    //   })
-    // );
-  }
-
-  function handleToggleMyList2(artworkId, nextSeen) {
-    updateMyList2((draft) => {
-      const artwork = draft.find((a) => a.id === artworkId);
-      artwork.seen = nextSeen;
-    });
-
-    // setMyList2(
-    //   myList2.map((artwork) => {
-    //     if (artwork.id === artworkId) {
-    //       return { ...artwork, seen: nextSeen };
-    //     } else {
-    //       return artwork;
-    //     }
-    //   })
-    // );
+  function handleIncreaseClick(productId) {
+    setProducts(
+      products.map((product) => {
+        if (product.id === productId) {
+          return { ...product, count: product.count + 1 };
+        } else {
+          return product;
+        }
+      })
+    );
   }
 
   return (
-    <>
-      <h1>Art Bucket List</h1>
-      <h2>My list of art to see:</h2>
-      <ItemList artworks={myList} onToggle={handleToggleMyList} />
-      <hr />
-      <ItemList2 artworks={myList2} onToggle={handleToggleMyList2} />
-    </>
-  );
-}
-
-function ItemList({ artworks, onToggle }) {
-  return (
     <ul>
-      {artworks.map((artwork) => (
-        <li key={artwork.id}>
-          <label>
-            <input
-              type="checkbox"
-              checked={artwork.seen}
-              onChange={(e) => {
-                onToggle(artwork.id, e.target.checked);
-              }}
-            />
-            {artwork.title}
-          </label>
-        </li>
-      ))}
-    </ul>
-  );
-}
-
-function ItemList2({ artworks, onToggle }) {
-  return (
-    <ul>
-      {artworks.map((artwork) => (
-        <li key={artwork.id}>
-          <label>
-            <input
-              type="checkbox"
-              checked={artwork.seen}
-              onChange={(e) => onToggle(artwork.id, e.target.checked)}
-            />
-            {artwork.title}
-          </label>
+      {products.map((product) => (
+        <li key={product.id}>
+          {product.name} (<b>{product.count}</b>)
+          <button
+            onClick={() => {
+              handleIncreaseClick(product.id);
+            }}
+          >
+            +
+          </button>
         </li>
       ))}
     </ul>
