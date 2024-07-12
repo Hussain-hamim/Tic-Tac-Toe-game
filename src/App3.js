ac41bdb3d82ceabc6b42070a65ae4452c7b5bc32
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-12 21:57:48 +0430

    challenge 4/5: clear an image while it's loading

--- src/App3.js
+++ src/App3.js
@@ -1,44 +1,61 @@
 import { useState } from "react";
-import ContactList from "./ContactList.js";
-import EditContact from "./EditContact.js";
+import first from "./hamim2.jpg";
+import second from "./hhamim.jpg";
 
-export default function ContactManager() {
-  const [contacts, setContacts] = useState(initialContacts);
-  const [selectedId, setSelectedId] = useState(0);
-  const selectedContact = contacts.find((c) => c.id === selectedId);
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const hasNext = index < images.length - 1;
 
-  console.log(selectedContact);
+  console.log(hasNext);
 
-  function handleSave(updatedData) {
-    const nextContacts = contacts.map((c) => {
-      if (c.id === updatedData.id) {
-        return updatedData;
-      } else {
-        return c;
-      }
-    });
-    setContacts(nextContacts);
+  function handleClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
   }
 
+  let image = images[index];
   return (
-    <div>
-      <ContactList
-        contacts={contacts}
-        selectedId={selectedId}
-        onSelect={(id) => setSelectedId(id)}
-      />
-      <hr />
-      <EditContact
-        key={selectedId}
-        initialData={selectedContact}
-        onSave={handleSave}
-      />
-    </div>
+    <>
+      <button onClick={handleClick}>Next</button>
+      <h3>
+        Image {index + 1} of {images.length}
+      </h3>
+      <img key={index} alt=".." src={image.src} width={200} />
+      <p>{image.place}</p>
+    </>
   );
 }
 
-const initialContacts = [
-  { id: 0, name: "Taylor", email: "taylor@mail.com" },
-  { id: 1, name: "Alice", email: "alice@mail.com" },
-  { id: 2, name: "Bob", email: "bob@mail.com" },
+let images = [
+  {
+    place: "Penang, Malaysia",
+    src: first,
+  },
+  {
+    place: "Lisbon, Portugal",
+    src: second,
+  },
+  {
+    place: "Bilbao, Spain",
+    src: first,
+  },
+  {
+    place: "Valparaíso, Chile",
+    src: second,
+  },
+  {
+    place: "Schwyz, Switzerland",
+    src: first,
+  },
+  {
+    place: "Prague, Czechia",
+    src: second,
+  },
+  {
+    place: "Ljubljana, Slovenia",
+    src: first,
+  },
 ];
