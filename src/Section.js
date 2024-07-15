c7fd7a2d47f515768e635d34a8bb205bb196531f
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-15 18:55:03 +0430

    context passes through intermediate components

--- src/Section.js
+++ src/Section.js
@@ -1,10 +1,13 @@
-import { LevelContext } from "./LevelContext";
+import { useContext } from "react";
+import { LevelContext } from "./LevelContext.js";
 
-export default function Section({ level, children }) {
+export default function Section({ children, isFancy }) {
+  const level = useContext(LevelContext);
   return (
-    // provide context
-    <section className="section">
-      <LevelContext.Provider value={level}>{children}</LevelContext.Provider>
+    <section className={"section " + (isFancy ? "fancy" : "")}>
+      <LevelContext.Provider value={level + 1}>
+        {children}
+      </LevelContext.Provider>
     </section>
   );
 }
