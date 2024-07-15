9b71d90addaeda5eb40659847a84ef7d9c80b6c3
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-15 21:36:08 +0430

    what's context and reducers

--- src/App3.js
+++ src/App3.js
@@ -1,66 +1,10 @@
-import { useContext, useState } from "react";
-import { places } from "./data.js";
-import { getImageUrl } from "./utils.js";
-import { ImageContext } from "./Context.js";
+import React from "react";
 
-export default function App() {
-  const [isLarge, setIsLarge] = useState(false);
-  const imageSize = isLarge ? 150 : 100;
+const App3 = () => {
+  // Reducers let you consolidate a component’s state update logic.
+  // Context lets you pass information deep down to other
 
-  return (
-    <>
-      <label>
-        <input
-          type="checkbox"
-          checked={isLarge}
-          onChange={(e) => {
-            setIsLarge(e.target.checked);
-          }}
-        />
-        Use large images
-      </label>
-      <hr />
+  return <div>App3</div>;
+};
 
-      {/* // provide context */}
-      <ImageContext.Provider value={imageSize}>
-        <List />
-      </ImageContext.Provider>
-    </>
-  );
-}
-
-function List() {
-  const listItems = places.map((place) => (
-    <li key={place.id}>
-      <Place place={place} />
-    </li>
-  ));
-  return <ul>{listItems}</ul>;
-}
-
-function Place({ place }) {
-  return (
-    <>
-      <PlaceImage place={place} />
-      <p>
-        <b>{place.name}</b>
-        {": " + place.description}
-      </p>
-    </>
-  );
-}
-
-function PlaceImage({ place }) {
-  // use context
-  const imageSize = useContext(ImageContext);
-
-  return (
-    <img
-      style={{ borderRadius: "20px", borderStyle: "dashed" }}
-      src={getImageUrl(place)}
-      alt={place.name}
-      width={imageSize}
-      height={imageSize}
-    />
-  );
-}
+export default App3;
