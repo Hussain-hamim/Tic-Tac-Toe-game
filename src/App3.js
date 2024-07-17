05e77d5fc1615527fc231d1fbfb51a6685ab4a85
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-17 09:48:28 +0430

    useRef...

--- src/App3.js
+++ src/App3.js
@@ -1,20 +1,12 @@
-import AddTask from "./AddTask.js";
-import TaskList from "./TaskList.js";
-import TasksProvider from "./TasksContext.js";
+import { useRef } from "react";
 
-// Reducers let you consolidate a component’s state update logic.
-// Context lets you pass information deep down to other components
-/** Here is how you can combine a reducer with context:
-1. Create the context.
-2. Put state and dispatch into context.
-3. Use context anywhere in the tree. */
+export default function Counter() {
+  let ref = useRef(0);
 
-export default function TaskApp() {
-  return (
-    <TasksProvider>
-      <h1>Day off in Kyoto</h1>
-      <AddTask />
-      <TaskList />
-    </TasksProvider>
-  );
+  function handleClick() {
+    ref.current = ref.current + 1;
+    alert("You clicked " + ref.current + " times!");
+  }
+
+  return <button onClick={handleClick}>Click me!</button>;
 }
