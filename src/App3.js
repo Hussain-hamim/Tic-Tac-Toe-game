7b435e4a5a5c1857d825b48e297062af1d43acdc
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-17 12:14:20 +0430

    practicing the challenge

--- src/App3.js
+++ src/App3.js
@@ -1,7 +1,35 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
-// Inside of React
-function useRef(initialValue) {
-  const [ref, unused] = useState({ current: initialValue });
-  return ref;
+export default function Chat() {
+  const [text, setText] = useState("");
+  const [isSending, setIsSending] = useState(false);
+  let timeoutID = useRef(null);
+
+  function handleSend() {
+    setIsSending(true);
+    timeoutID.current = setTimeout(() => {
+      alert("sent!");
+      setIsSending(false);
+    }, 3000);
+  }
+
+  function handleUndo() {
+    setIsSending(false);
+    clearTimeout(timeoutID.current);
+  }
+
+  return (
+    <>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button onClick={handleSend} disabled={isSending}>
+        {isSending ? "Sending..." : "Send"}
+      </button>
+
+      {isSending && <button onClick={handleUndo}>Undo</button>}
+    </>
+  );
 }
