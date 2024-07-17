9b31275d632ba097eac9428ceea7919f3f1fc8f3
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-17 10:14:25 +0430

    stopWatch with ref

--- src/App3.js
+++ src/App3.js
@@ -1,12 +1,39 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
-export default function Counter() {
-  let ref = useRef(0);
+export default function Stopwatch() {
+  const [startTime, setStartTime] = useState(null);
+  const [now, setNow] = useState(null);
+  const intervalRef = useRef(null);
 
-  function handleClick() {
-    ref.current = ref.current + 1;
-    alert("You clicked " + ref.current + " times!");
+  function handleStart() {
+    // Start counting.
+    setStartTime(Date.now());
+    setNow(Date.now());
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(() => {
+      // Update the current time every 10ms.
+      setNow(Date.now());
+    }, 10);
   }
 
-  return <button onClick={handleClick}>Click me!</button>;
+  function handleStop() {
+    clearInterval(intervalRef.current);
+  }
+
+  let secondsPassed = 0;
+  if (startTime != null && now != null) {
+    secondsPassed = (now - startTime) / 1000;
+  }
+
+  console.log("now: ", now);
+  console.log("start time: ", startTime);
+  console.log("sp: ", secondsPassed);
+
+  return (
+    <>
+      <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+    </>
+  );
 }
