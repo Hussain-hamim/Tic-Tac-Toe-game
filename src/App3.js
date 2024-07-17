331be8ddbfaaea4f6f5d77f14ec74a486d96535e
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-17 14:22:27 +0430

    challenge 1/4: fix a broken chat input

--- src/App3.js
+++ src/App3.js
@@ -1,34 +1,33 @@
-import { useRef, useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Chat() {
   const [text, setText] = useState("");
   const [isSending, setIsSending] = useState(false);
-  let timeoutID = useRef(null);
+  const timeoutRef = useRef(null);
 
   function handleSend() {
     setIsSending(true);
-    timeoutID.current = setTimeout(() => {
-      alert("sent!");
+    timeoutRef.current = setTimeout(() => {
+      alert("Sent!");
       setIsSending(false);
     }, 3000);
   }
 
   function handleUndo() {
     setIsSending(false);
-    clearTimeout(timeoutID.current);
+    clearTimeout(timeoutRef.current);
   }
 
   return (
     <>
       <input
-        type="text"
+        disabled={isSending}
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
-      <button onClick={handleSend} disabled={isSending}>
+      <button disabled={isSending} onClick={handleSend}>
         {isSending ? "Sending..." : "Send"}
       </button>
-
       {isSending && <button onClick={handleUndo}>Undo</button>}
     </>
   );
