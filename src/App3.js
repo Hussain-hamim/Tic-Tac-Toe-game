c6c48a437be09ddf72b1f404fce59a5d813dd646
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-17 10:23:54 +0430

    reading ref during render lead to unreliable code

--- src/App3.js
+++ src/App3.js
@@ -1,39 +1,14 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 
-export default function Stopwatch() {
-  const [startTime, setStartTime] = useState(null);
-  const [now, setNow] = useState(null);
-  const intervalRef = useRef(null);
+export default function Counter() {
+  let countRef = useRef(0);
 
-  function handleStart() {
-    // Start counting.
-    setStartTime(Date.now());
-    setNow(Date.now());
-    clearInterval(intervalRef.current);
-    intervalRef.current = setInterval(() => {
-      // Update the current time every 10ms.
-      setNow(Date.now());
-    }, 10);
+  function handleClick() {
+    // This doesn't re-render the component!
+    countRef.current = countRef.current + 1;
   }
-
-  function handleStop() {
-    clearInterval(intervalRef.current);
-  }
-
-  let secondsPassed = 0;
-  if (startTime != null && now != null) {
-    secondsPassed = (now - startTime) / 1000;
-  }
-
-  console.log("now: ", now);
-  console.log("start time: ", startTime);
-  console.log("sp: ", secondsPassed);
 
   return (
-    <>
-      <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
-      <button onClick={handleStart}>Start</button>
-      <button onClick={handleStop}>Stop</button>
-    </>
+    <button onClick={handleClick}>You clicked {countRef.current} times</button>
   );
 }
