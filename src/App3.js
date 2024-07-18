cf5d68aaab7f497c48319f1b7c519d6f2a866bb6
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-18 14:57:58 +0430

    hardest deep dive about list of refs

--- src/App3.js
+++ src/App3.js
@@ -1,121 +1,63 @@
-import React, { useRef } from "react";
+import { useRef, useState } from "react";
+import second from "./hhamim.jpg";
 
-const App3 = () => {
-  const one = useRef(null);
+export default function CatFriends() {
+  const itemsRef = useRef(null);
+  const [catList, setCatList] = useState(setupCatList);
 
-  function scroll() {
-    one.current.scrollIntoView({
-      //
+  function scrollToCat(cat) {
+    const map = getMap();
+    const node = map.get(cat);
+    node.scrollIntoView({
       behavior: "smooth",
       block: "nearest",
       inline: "center",
     });
   }
 
+  function getMap() {
+    if (!itemsRef.current) {
+      // Initialize the Map on first usage.
+      itemsRef.current = new Map();
+    }
+    return itemsRef.current;
+  }
+
   return (
     <>
-      <button onClick={scroll}>scroll</button>
-
+      <nav>
+        <button onClick={() => scrollToCat(catList[0])}>Tom</button>
+        <button onClick={() => scrollToCat(catList[5])}>Maru</button>
+        <button onClick={() => scrollToCat(catList[9])}>Jellylorum</button>
+      </nav>
       <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci autem
-        qui vitae est voluptate perspiciatis totam reiciendis commodi iusto
-        deserunt, consequuntur reprehenderit dignissimos, neque suscipit eum
-        quisquam omnis. Suscipit, at. Earum quod totam culpa, ullam et
-        accusamus. Ab inventore repellendus nihil. Numquam, laudantium sint
-        corporis laboriosam, quis dolor, necessitatibus excepturi saepe eos
-        reiciendis molestiae quos aspernatur error a. Vel expedita nostrum ut
-        aspernatur magni reiciendis atque hic possimus totam aliquid. Tempora
-        incidunt eum ex similique. Blanditiis eos corporis, dolorem enim
-        cupiditate suscipit, saepe repellat magni voluptatum et consequuntur
-        assumenda nostrum nemo, commodi sapiente eveniet sunt impedit. Ipsa
-        sapiente at iusto?
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci autem
-        qui vitae est voluptate perspiciatis totam reiciendis commodi iusto
-        deserunt, consequuntur reprehenderit dignissimos, neque suscipit eum
-        quisquam omnis. Suscipit, at. Earum quod totam culpa, ullam et
-        accusamus. Ab inventore repellendus nihil. Numquam, laudantium sint
-        corporis laboriosam, quis dolor, necessitatibus excepturi saepe eos
-        reiciendis molestiae quos aspernatur error a. Vel expedita nostrum ut
-        aspernatur magni reiciendis atque hic possimus totam aliquid. Tempora
-        incidunt eum ex similique. Blanditiis eos corporis, dolorem enim
-        cupiditate suscipit, saepe repellat magni voluptatum et consequuntur
-        assumenda nostrum nemo, commodi sapiente eveniet sunt impedit. Ipsa
-        sapiente at iusto?
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci autem
-        qui vitae est voluptate perspiciatis totam reiciendis commodi iusto
-        deserunt, consequuntur reprehenderit dignissimos, neque suscipit eum
-        quisquam omnis. Suscipit, at. Earum quod totam culpa, ullam et
-        accusamus. Ab inventore repellendus nihil. Numquam, laudantium sint
-        corporis laboriosam, quis dolor, necessitatibus excepturi saepe eos
-        reiciendis molestiae quos aspernatur error a. Vel expedita nostrum ut
-        aspernatur magni reiciendis atque hic possimus totam aliquid. Tempora
-        incidunt eum ex similique. Blanditiis eos corporis, dolorem enim
-        cupiditate suscipit, saepe repellat magni voluptatum et consequuntur
-        assumenda nostrum nemo, commodi sapiente eveniet sunt impedit. Ipsa
-        sapiente at iusto?
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci autem
-        qui vitae est voluptate perspiciatis totam reiciendis commodi iusto
-        deserunt, consequuntur reprehenderit dignissimos, neque suscipit eum
-        quisquam omnis. Suscipit, at. Earum quod totam culpa, ullam et
-        accusamus. Ab inventore repellendus nihil. Numquam, laudantium sint
-        corporis laboriosam, quis dolor, necessitatibus excepturi saepe eos
-        reiciendis molestiae quos aspernatur error a. Vel expedita nostrum ut
-        aspernatur magni reiciendis atque hic possimus totam aliquid. Tempora
-        incidunt eum ex similique. Blanditiis eos corporis, dolorem enim
-        cupiditate suscipit, saepe repellat magni voluptatum et consequuntur
-        assumenda nostrum nemo, commodi sapiente eveniet sunt impedit. Ipsa
-        sapiente at iusto?
-      </div>
-      <div style={{ color: "orchid" }} ref={one}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci autem
-        qui vitae est voluptate perspiciatis totam reiciendis commodi iusto
-        deserunt, consequuntur reprehenderit dignissimos, neque suscipit eum
-        quisquam omnis. Suscipit, at. Earum quod totam culpa, ullam et
-        accusamus. Ab inventore repellendus nihil. Numquam, laudantium sint
-        corporis laboriosam, quis dolor, necessitatibus excepturi saepe eos
-        reiciendis molestiae quos aspernatur error a. Vel expedita nostrum ut
-        aspernatur magni reiciendis atque hic possimus totam aliquid. Tempora
-        incidunt eum ex similique. Blanditiis eos corporis, dolorem enim
-        cupiditate suscipit, saepe repellat magni voluptatum et consequuntur
-        assumenda nostrum nemo, commodi sapiente eveniet sunt impedit. Ipsa
-        sapiente at iusto?
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci autem
-        qui vitae est voluptate perspiciatis totam reiciendis commodi iusto
-        deserunt, consequuntur reprehenderit dignissimos, neque suscipit eum
-        quisquam omnis. Suscipit, at. Earum quod totam culpa, ullam et
-        accusamus. Ab inventore repellendus nihil. Numquam, laudantium sint
-        corporis laboriosam, quis dolor, necessitatibus excepturi saepe eos
-        reiciendis molestiae quos aspernatur error a. Vel expedita nostrum ut
-        aspernatur magni reiciendis atque hic possimus totam aliquid. Tempora
-        incidunt eum ex similique. Blanditiis eos corporis, dolorem enim
-        cupiditate suscipit, saepe repellat magni voluptatum et consequuntur
-        assumenda nostrum nemo, commodi sapiente eveniet sunt impedit. Ipsa
-        sapiente at iusto?
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci autem
-        qui vitae est voluptate perspiciatis totam reiciendis commodi iusto
-        deserunt, consequuntur reprehenderit dignissimos, neque suscipit eum
-        quisquam omnis. Suscipit, at. Earum quod totam culpa, ullam et
-        accusamus. Ab inventore repellendus nihil. Numquam, laudantium sint
-        corporis laboriosam, quis dolor, necessitatibus excepturi saepe eos
-        reiciendis molestiae quos aspernatur error a. Vel expedita nostrum ut
-        aspernatur magni reiciendis atque hic possimus totam aliquid. Tempora
-        incidunt eum ex similique. Blanditiis eos corporis, dolorem enim
-        cupiditate suscipit, saepe repellat magni voluptatum et consequuntur
-        assumenda nostrum nemo, commodi sapiente eveniet sunt impedit. Ipsa
-        sapiente at iusto?
+        <ul>
+          {catList.map((cat) => (
+            <li
+              key={cat}
+              ref={(node) => {
+                const map = getMap();
+                if (node) {
+                  map.set(cat, node);
+                } else {
+                  map.delete(cat);
+                }
+              }}
+            >
+              <img src={cat} />
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
-};
+}
 
-export default App3;
+function setupCatList() {
+  const catList = [];
+  for (let i = 0; i < 10; i++) {
+    catList.push(second);
+  }
+
+  return catList;
+}
