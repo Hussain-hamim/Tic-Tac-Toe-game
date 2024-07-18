cafa5a2895839b04ce192e8f9c8d5a9c9e73622e
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-18 15:30:09 +0430

    assigning another comp dom node

--- src/App3.js
+++ src/App3.js
@@ -1,63 +1,27 @@
-import { useRef, useState } from "react";
-import second from "./hhamim.jpg";
+import { forwardRef, useRef } from "react";
 
-export default function CatFriends() {
-  const itemsRef = useRef(null);
-  const [catList, setCatList] = useState(setupCatList);
+const MyInput = forwardRef((props, ref) => {
+  return <input {...props} ref={ref} />;
+});
 
-  function scrollToCat(cat) {
-    const map = getMap();
-    const node = map.get(cat);
-    node.scrollIntoView({
-      behavior: "smooth",
-      block: "nearest",
-      inline: "center",
-    });
-  }
+export default function Form() {
+  const inputRef = useRef(null);
 
-  function getMap() {
-    if (!itemsRef.current) {
-      // Initialize the Map on first usage.
-      itemsRef.current = new Map();
-    }
-    return itemsRef.current;
+  function handleClick() {
+    inputRef.current.focus();
   }
 
   return (
     <>
-      <nav>
-        <button onClick={() => scrollToCat(catList[0])}>Tom</button>
-        <button onClick={() => scrollToCat(catList[5])}>Maru</button>
-        <button onClick={() => scrollToCat(catList[9])}>Jellylorum</button>
-      </nav>
-      <div>
-        <ul>
-          {catList.map((cat) => (
-            <li
-              key={cat}
-              ref={(node) => {
-                const map = getMap();
-                if (node) {
-                  map.set(cat, node);
-                } else {
-                  map.delete(cat);
-                }
-              }}
-            >
-              <img src={cat} />
-            </li>
-          ))}
-        </ul>
-      </div>
+      <MyInput ref={inputRef} />
+      {/* <input type="text" ref={inputRef} /> */}
+      <button onClick={handleClick}>Focus the input</button>
+
+      {/* <HisInput ref={inputRef} /> */}
     </>
   );
 }
 
-function setupCatList() {
-  const catList = [];
-  for (let i = 0; i < 10; i++) {
-    catList.push(second);
-  }
-
-  return catList;
-}
+const HisInput = forwardRef((props, ref) => {
+  return <input ref={ref} {...props} />;
+});
