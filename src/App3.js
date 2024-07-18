b6816328b3c351cd0e943331034e6b6878528542
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-18 16:13:38 +0430

    exposing a subset of the api with an imperative handle

--- src/App3.js
+++ src/App3.js
@@ -1,7 +1,15 @@
-import { forwardRef, useRef } from "react";
+import { forwardRef, useRef, useImperativeHandle } from "react";
 
 const MyInput = forwardRef((props, ref) => {
-  return <input {...props} ref={ref} />;
+  const realInputRef = useRef(null);
+  useImperativeHandle(ref, () => ({
+    // Only expose focus and nothing else
+    focus() {
+      realInputRef.current.focus();
+      // realInputRef.current.style.backgroundColor = "orchid";
+    },
+  }));
+  return <input {...props} ref={realInputRef} />;
 });
 
 export default function Form() {
@@ -15,14 +23,7 @@ export default function Form() {
   return (
     <>
       <MyInput ref={inputRef} />
-      {/* <input type="text" ref={inputRef} /> */}
       <button onClick={handleClick}>Focus the input</button>
-
-      {/* <HisInput ref={inputRef} /> */}
     </>
   );
 }
-
-const HisInput = forwardRef((props, ref) => {
-  return <input ref={ref} {...props} />;
-});
