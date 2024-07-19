7bb66dd9eb0ebbe5b0ac14254fd6c0f3b4bbdce4
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-19 11:00:22 +0430

    adding clean fn to effect

--- src/App3.js
+++ src/App3.js
@@ -1,34 +1,18 @@
-import { useState, useRef, useEffect } from "react";
-import second from "./quran.mp4";
+import React, { useEffect } from "react";
+import { createConnection } from "./chat2";
 
-function VideoPlayer({ src, isPlaying }) {
-  const ref = useRef(null);
-
+const App3 = () => {
   useEffect(() => {
-    if (isPlaying) {
-      console.log("Calling video.play()");
-      ref.current.play();
-    } else {
-      console.log("Calling video.pause()");
-      ref.current.pause();
-    }
-  }, [isPlaying]);
+    const connection = createConnection();
+    connection.connect();
 
-  return <video width={"200px"} ref={ref} src={src} loop playsInline />;
-}
+    // cleanup fn
+    return () => {
+      connection.disconnect();
+    };
+  }, []);
 
-export default function App() {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [text, setText] = useState("");
-  return (
-    <>
-      <input value={text} onChange={(e) => setText(e.target.value)} />
-      <button onClick={() => setIsPlaying(!isPlaying)}>
-        {isPlaying ? "Pause" : "Play"}
-      </button>
-      <br />
-      <br />
-      <VideoPlayer isPlaying={isPlaying} src={second} />
-    </>
-  );
-}
+  return <div>welcome to the chat</div>;
+};
+
+export default App3;
