46e8b6d455ae5ce9281f71ac58b3a476fab27e41
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-19 22:22:36 +0430

    challenge 3/4: fix an interval that fires twice

--- src/App3.js
+++ src/App3.js
@@ -1,35 +1,20 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useState, useEffect } from "react";
 
-const App3 = () => {
-  const [text, setText] = useState("");
-  const [focus, setFocus] = useState(false);
-
-  function handleChange(e) {
-    setText(e.target.value);
-  }
-
-  return (
-    <div>
-      <MyInput shouldFocus={!focus} value={text} onChange={handleChange} />
-      <MyInput shouldFocus={focus} value={text} onChange={handleChange} />
-      <button onClick={() => setFocus(true)}>Focus</button>
-    </div>
-  );
-};
-export default App3;
-
-const MyInput = ({ shouldFocus, value, onChange }) => {
-  const ref = useRef(null);
+export default function Counter() {
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    if (shouldFocus) {
-      ref.current.focus();
+    function onTick() {
+      setCount((c) => c + 1);
     }
-  }, [shouldFocus]);
 
-  return (
-    <div>
-      <input ref={ref} value={value} onChange={onChange} />
-    </div>
-  );
-};
+    const intervalId = setInterval(onTick, 1000);
+
+    // cleanup fn
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return <h1>{count}</h1>;
+}
