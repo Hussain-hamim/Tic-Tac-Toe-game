1af7da19e7ca7fad77da183135427a973b0f856f
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-19 10:38:18 +0430

    adding dependency array for effect

--- src/App3.js
+++ src/App3.js
@@ -1,33 +1,34 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import second from "./quran.mp4";
-
-const App3 = () => {
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  return (
-    <div>
-      <button onClick={() => setIsPlaying(!isPlaying)}>
-        {isPlaying ? "Pause" : "Play"}
-      </button>
-      <VideoPlayer isPlaying={isPlaying} src={second} />
-    </div>
-  );
-};
-export default App3;
 
 function VideoPlayer({ src, isPlaying }) {
   const ref = useRef(null);
 
-  //wrap the side effect with useEffect to move it out of
-  // the rendering calculation
   useEffect(() => {
     if (isPlaying) {
+      console.log("Calling video.play()");
       ref.current.play();
     } else {
+      console.log("Calling video.pause()");
       ref.current.pause();
     }
-  });
+  }, [isPlaying]);
 
-  // TODO: do something here
-  return <video width={"200px"} ref={ref} src={src} />;
+  return <video width={"200px"} ref={ref} src={src} loop playsInline />;
+}
+
+export default function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [text, setText] = useState("");
+  return (
+    <>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={() => setIsPlaying(!isPlaying)}>
+        {isPlaying ? "Pause" : "Play"}
+      </button>
+      <br />
+      <br />
+      <VideoPlayer isPlaying={isPlaying} src={second} />
+    </>
+  );
 }
