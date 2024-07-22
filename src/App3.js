379e7e7eaeed8eab6502469be5ce7f5a2f5ddb32
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-22 17:03:22 +0430

    challenge 1/4: transform data without effects

--- src/App3.js
+++ src/App3.js
@@ -1,40 +1,49 @@
-import { useState, useEffect } from "react";
-import { fetchBio } from "./api.js";
+import { useState } from "react";
+import { initialTodos, createTodo } from "./todos.js";
 
-export default function Page() {
-  const [person, setPerson] = useState("Hussain");
-  const [bio, setBio] = useState(null);
-  useEffect(() => {
-    let ignore = false;
+export default function TodoList() {
+  const [todos, setTodos] = useState(initialTodos);
+  const [showActive, setShowActive] = useState(false);
 
-    setBio(null);
-    fetchBio(person).then((result) => {
-      if (!ignore) {
-        setBio(result);
-      }
-    });
-    // cleanup fn
-    return () => {
-      ignore = true;
-    };
-  }, [person]);
+  const activeTodos = todos.filter((todo) => !todo.completed);
+  const visibleTodos = showActive ? activeTodos : todos;
 
   return (
     <>
-      <select
-        value={person}
-        onChange={(e) => {
-          setPerson(e.target.value);
-        }}
-      >
-        <option value="Hussain">Hussain</option>
-        <option value="Hamim">Hamim</option>
-        <option value="Haroon">Haroon</option>
-      </select>
-      <hr />
-      <p>
-        <i>{bio ?? "Loading..."}</i>
-      </p>
+      <label>
+        <input
+          type="checkbox"
+          checked={showActive}
+          onChange={(e) => setShowActive(e.target.checked)}
+        />
+        Show only active todos
+      </label>
+      <NewTodo onAdd={(newTodo) => setTodos([...todos, newTodo])} />
+      <ul>
+        {visibleTodos.map((todo) => (
+          <li key={todo.id}>
+            {todo.completed ? <s>{todo.text}</s> : todo.text}
+          </li>
+        ))}
+      </ul>
+
+      <footer>{activeTodos.length} todos left</footer>
+    </>
+  );
+}
+
+function NewTodo({ onAdd }) {
+  const [text, setText] = useState("");
+
+  function handleAddClick() {
+    setText("");
+    onAdd(createTodo(text));
+  }
+
+  return (
+    <>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={handleAddClick}>Add</button>
     </>
   );
 }
