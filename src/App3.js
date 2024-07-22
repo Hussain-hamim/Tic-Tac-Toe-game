af7ec01604681dface2682c14d8bd515658bb44d
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-22 20:07:05 +0430

    challenge 2/4: cache a calculation without effects.

--- src/App3.js
+++ src/App3.js
@@ -1,12 +1,20 @@
-import { useState } from "react";
-import { initialTodos, createTodo } from "./todos.js";
+import { useState, useEffect, useMemo } from "react";
+import { initialTodos, createTodo, getVisibleTodos } from "./todos.js";
 
 export default function TodoList() {
   const [todos, setTodos] = useState(initialTodos);
   const [showActive, setShowActive] = useState(false);
+  const [text, setText] = useState("");
 
-  const activeTodos = todos.filter((todo) => !todo.completed);
-  const visibleTodos = showActive ? activeTodos : todos;
+  const visibleTodos = useMemo(
+    () => getVisibleTodos(todos, showActive),
+    [todos, showActive]
+  );
+
+  function handleAddClick() {
+    setText("");
+    setTodos([...todos, createTodo(text)]);
+  }
 
   return (
     <>
@@ -14,11 +22,14 @@ export default function TodoList() {
         <input
           type="checkbox"
           checked={showActive}
-          onChange={(e) => setShowActive(e.target.checked)}
+          onChange={(e) => {
+            setShowActive(e.target.checked);
+          }}
         />
         Show only active todos
       </label>
-      <NewTodo onAdd={(newTodo) => setTodos([...todos, newTodo])} />
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={handleAddClick}>Add</button>
       <ul>
         {visibleTodos.map((todo) => (
           <li key={todo.id}>
@@ -26,24 +37,6 @@ export default function TodoList() {
           </li>
         ))}
       </ul>
-
-      <footer>{activeTodos.length} todos left</footer>
-    </>
-  );
-}
-
-function NewTodo({ onAdd }) {
-  const [text, setText] = useState("");
-
-  function handleAddClick() {
-    setText("");
-    onAdd(createTodo(text));
-  }
-
-  return (
-    <>
-      <input value={text} onChange={(e) => setText(e.target.value)} />
-      <button onClick={handleAddClick}>Add</button>
     </>
   );
 }
