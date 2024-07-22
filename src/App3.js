fa7b9d7eee277356577dbac43070e10716c4f7fd
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-22 22:17:00 +0430

    What an Effect with empty dependencies means

--- src/App3.js
+++ src/App3.js
@@ -1,43 +1,27 @@
 import { useState, useEffect } from "react";
-import { createConnection } from "./chat3.js";
+import { createConnection } from "./chat.js";
 
-function ChatRoom({ roomId }) {
-  const [serverUrl, setServerUrl] = useState("https://localhost:1234");
+const serverUrl = "https://localhost:1234";
+const roomId = "general";
 
+function ChatRoom() {
   useEffect(() => {
     const connection = createConnection(serverUrl, roomId);
     connection.connect();
     return () => connection.disconnect();
-  }, [roomId, serverUrl]);
-
-  return (
-    <>
-      <label>
-        Server URL:{" "}
-        <input
-          value={serverUrl}
-          onChange={(e) => setServerUrl(e.target.value)}
-        />
-      </label>
-      <h1>Welcome to the {roomId} room!</h1>
-    </>
-  );
+  }, []);
+  return <h1>Welcome to the {roomId} room!</h1>;
 }
 
 export default function App() {
-  const [roomId, setRoomId] = useState("general");
+  const [show, setShow] = useState(false);
   return (
     <>
-      <label>
-        Choose the chat room:{" "}
-        <select value={roomId} onChange={(e) => setRoomId(e.target.value)}>
-          <option value="general">general</option>
-          <option value="travel">travel</option>
-          <option value="music">music</option>
-        </select>
-      </label>
-      <hr />
-      <ChatRoom roomId={roomId} />
+      <button onClick={() => setShow(!show)}>
+        {show ? "Close chat" : "Open chat"}
+      </button>
+      {show && <hr />}
+      {show && <ChatRoom />}
     </>
   );
 }
