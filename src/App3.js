8c8c246fc029b8df8eb87c981c4dbdd49427efc5
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-26 15:05:11 +0430

    Challenge 1 of 4: Fix a resetting interval

--- src/App3.js
+++ src/App3.js
@@ -1,47 +1,19 @@
 import { useState, useEffect } from "react";
-import { createConnection } from "./chat.js";
 
-function ChatRoom({ getOptions }) {
-  const [message, setMessage] = useState("");
-
-  const { roomId, serverUrl } = getOptions();
-
-  /**This only works for pure functions because they are safe to call
-   *  during rendering. If your function is an event handler,
-   *  but you don’t want its changes to re-synchronize your Effect,
-   *  wrap it into an Effect Event instead. */
+export default function Timer() {
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    const connection = createConnection({
-      roomId: roomId,
-      serverUrl: serverUrl,
-    });
-    connection.connect();
-    return () => connection.disconnect();
-  }, [roomId, serverUrl]);
+    console.log("✅ Creating an interval");
+    const id = setInterval(() => {
+      console.log("⏰ Interval tick");
+      setCount((c) => c + 1);
+    }, 1000);
+    return () => {
+      console.log("❌ Clearing an interval");
+      clearInterval(id);
+    };
+  }, []);
 
-  return (
-    <>
-      <h1>Welcome to the {roomId} room!</h1>
-      <input value={message} onChange={(e) => setMessage(e.target.value)} />
-    </>
-  );
-}
-
-export default function App() {
-  const [roomId, setRoomId] = useState("general");
-  return (
-    <>
-      <label>
-        Choose the chat room:{" "}
-        <select value={roomId} onChange={(e) => setRoomId(e.target.value)}>
-          <option value="general">general</option>
-          <option value="travel">travel</option>
-          <option value="music">music</option>
-        </select>
-      </label>
-      <hr />
-      <ChatRoom roomId={roomId} />
-    </>
-  );
+  return <h1>Counter: {count}</h1>;
 }
