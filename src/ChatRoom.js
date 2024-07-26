55caf34d8f38c99b778bcaafeddab457f73700e7
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-26 08:53:33 +0430

    event handler prop may go inside effect event

--- src/ChatRoom.js
+++ src/ChatRoom.js
@@ -1,20 +1,12 @@
 import { useEffect, useState, useEffectEvent } from "react";
 
-function ChatRoom({ roomId, createConnection }) {
+function ChatRoom({ roomId, createConnection, onReceivedMessages }) {
   const [messages, setMessages] = useState([]);
   const [isMuted, setIsMuted] = useState(false);
 
   function playSound() {
     ////...
   }
-
-  /** Effect Events let you split an Effect into reactive parts
-   *  (which should “react” to reactive values like roomId
-   * and their changes) and non-reactive parts
-   * (which only read their latest values,
-   * like onMessage reads isMuted).
-   *  Now that you read isMuted inside an Effect Event,
-   * it doesn’t need to be a dependency of your Effect */
 
   const onMessage = useEffectEvent((receivedMessage) => {
     setMessages((msgs) => [...msgs, receivedMessage]);
@@ -23,14 +15,19 @@ function ChatRoom({ roomId, createConnection }) {
     }
   });
 
+  const onMessaging = useEffectEvent((receivedMsg) => {
+    onReceivedMessages(receivedMsg); // event handler props
+  });
+
   useEffect(() => {
     const connection = createConnection();
     connection.connect();
     connection.on("message", (receivedMessage) => {
       onMessage(receivedMessage);
+      onMessaging(receivedMessage);
     });
     return () => connection.disconnect();
-  }, [roomId]); // ✅ All dependencies declared
+  }, [createConnection, onMessage, roomId]); // ✅ All dependencies declared
   // ...
 
   return <h1>Welcome to the {roomId} room!</h1>;
