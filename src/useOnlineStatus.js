549deddf1a5b5d35edec7c6e5913a20c4d2dac36
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-28 09:15:47 +0430

    there is another way for it

--- src/useOnlineStatus.js
+++ src/useOnlineStatus.js
@@ -1,31 +1,59 @@
-// import { useSyncExternalStore } from "react";
+import { useState, useEffect, useRef } from "react";
 
-// function subscribe(callback) {
-//   window.addEventListener("online", callback);
-//   window.addEventListener("offline", callback);
-//   return () => {
-//     window.removeEventListener("online", callback);
-//     window.removeEventListener("offline", callback);
-//   };
-// }
+function Welcome() {
+  const ref = useRef(null);
 
-// export function useOnlineStatus() {
-//   return useSyncExternalStore(
-//     subscribe,
-//     () => navigator.onLine, // How to get the value on the client
-//     () => true // How to get the value on the server
-//   );
-// }
+  useEffect(() => {
+    const duration = 1000;
+    const node = ref.current;
 
-import { use, useState } from "react"; // Not available yet!
+    let startTime = performance.now();
+    let frameId = null;
 
-/** We’re still working out the details,
- * but we expect that in the future,
- *  you’ll write data fetching like this:  */
+    function onFrame(now) {
+      const timePassed = now - startTime;
+      const progress = Math.min(timePassed / duration, 1);
+      onProgress(progress);
+      if (progress < 1) {
+        // We still have more frames to paint
+        frameId = requestAnimationFrame(onFrame);
+      }
+    }
 
-function ShippingForm({ country }) {
-  const cities = use(fetch(`/api/cities?country=${country}`));
-  const [city, setCity] = useState(null);
-  const areas = city ? use(fetch(`/api/areas?city=${city}`)) : null;
-  // ...
+    function onProgress(progress) {
+      node.style.opacity = progress;
+    }
+
+    function start() {
+      onProgress(0);
+      startTime = performance.now();
+      frameId = requestAnimationFrame(onFrame);
+    }
+
+    function stop() {
+      cancelAnimationFrame(frameId);
+      startTime = null;
+      frameId = null;
+    }
+
+    start();
+    return () => stop();
+  }, []);
+
+  return (
+    <h1 className="welcome" ref={ref}>
+      Welcome
+    </h1>
+  );
+}
+
+export default function App() {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <button onClick={() => setShow(!show)}>{show ? "Remove" : "Show"}</button>
+      <hr />
+      {show && <Welcome />}
+    </>
+  );
 }
