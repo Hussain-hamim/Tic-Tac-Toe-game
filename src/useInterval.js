040eb144725b2c9ece34a067c2e82989e82e16b8
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-28 11:28:58 +0430

    there still a problem with this code one solution: arrow fn

--- src/useInterval.js
+++ src/useInterval.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export function useInterval(onTick, delay) {
   return useEffect(() => {
-    const id = setInterval(onTick(), delay);
+    const id = setInterval(() => onTick(), delay);
 
     return () => clearInterval(id);
   }, [delay, onTick]);
