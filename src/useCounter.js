65a3e6c9c109d0fb691edf2cea57b1b6aad3eee0
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-28 10:51:08 +0430

    challenge 1 solution

--- src/useCounter.js
+++ src/useCounter.js
@@ -1,1 +1,17 @@
 // write your custom hook here...
+
+import { useEffect, useState } from "react";
+
+export function useCounter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCount((c) => c + 1);
+    }, 1000);
+
+    return () => clearInterval(id);
+  }, []);
+
+  return count;
+}
