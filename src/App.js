bb581772c75e12b02d27768e7189b845c22eb498
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-07-28 11:14:55 +0430

    Challenge 2 of 5: Make the counter delay configurable

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
 import { useCounter } from "./useCounter";
 
 export default function Counter() {
+  const [delay, setDelay] = useState(1000);
   const count = useCounter();
 
-  return <h1>Seconds passed: {count}</h1>;
+  return (
+    <>
+      <label>
+        Tick Duration: {delay} ms
+        <br />
+        <input
+          type="range"
+          value={delay}
+          min={10}
+          max={2000}
+          onChange={(e) => setDelay(Number(e.target.value))}
+        />
+      </label>
+
+      <hr />
+      <h1>Seconds passed: {count}</h1>
+    </>
+  );
 }
